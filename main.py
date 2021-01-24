9cbc74787127482bf3af77eeabd735e4df89185d
Marcin Jędral <info.marcinjedral@gmail.com>
2021-01-24 22:01:54 +0000

    .github/workflows folder has been added

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import re
 
 cryptoClient = discord.Client()
 
+apikey = 'APIKEY'
 
 def get_btc_pln():
-  cryptourlbtc = 'https://coinlib.io/api/v1/coin?key=6e642f02665c6e3e&pref=PLN&symbol=BTC'
+  cryptourlbtc = f'https://coinlib.io/api/v1/coin?key=6e642f02665c6e3e&pref=PLN&symbol=BTC'
   response = requests.get(cryptourlbtc)
   json_data = json.loads(response.text)
   price = json_data["price"]
@@ -35,7 +36,7 @@ def get_btc_usd():
   response = requests.get('https://coinlib.io/api/v1/coin?key=6e642f02665c6e3e&pref=USD&symbol=BTC')
   json_data = json.loads(response.text)
   price = json_data["price"]
-  crcurusd = "Bitcoin price in PLN: " + re.sub(r'^(\d+\.\d{,2})\d*$',r'\1',str(price))
+  crcurusd = "Bitcoin price in USD: " + re.sub(r'^(\d+\.\d{,2})\d*$',r'\1',str(price))
   return crcurusd
 
 print(get_btc_pln())
@@ -44,11 +45,15 @@ print(get_btc_pln())
 async def on_ready():
   print(f'We have logged in as {cryptoClient.user}')
 
-
 @cryptoClient.event
 async def on_message(message):
+
   if (message.author == cryptoClient.user):
     return
+
+  if (message.content.startswith('$jasioka')):
+    crcurpln = get_btc_pln()
+    await message.channel.send("Czary mary hokus pokus jasioka to mały łobuz")
 
   if (message.content.startswith('$btcpln')):
     crcurpln = get_btc_pln()
